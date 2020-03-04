219f7c4d0695849cc03e4b1bb8517538b6d5eb71
rednaxela700 <ohleg@windowslive.com>
2020-03-04 21:40:59 +0100

    fixed paresInt calculations, added missing semicolons

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class Player {
     constructor(name, active) {
 
-        this.name = name
+        this.name = name;
         this.active = active
     }
     animals = {
@@ -12,7 +12,7 @@ class Player {
         horse: 0,
         sDog: 0,
         bDog: 0,
-    }
+    };
     countAnimals() {
         return Object.values(this.animals)
     }
@@ -20,7 +20,7 @@ class Player {
 
 class Board {
     constructor(domId, playersNum) {
-        this.domId = domId
+        this.domId = domId;
         this.playersNum = playersNum
     }
     getDomId() {
@@ -29,9 +29,9 @@ class Board {
 }
 
 class Game {
-    yellowDice = ["wolf", "cow", "sheep", "sheep", "sheep", "pig", "rabbit", "rabbit", "rabbit", "rabbit", "rabbit", "rabbit"]
+    yellowDice = ["wolf", "cow", "sheep", "sheep", "sheep", "pig", "rabbit", "rabbit", "rabbit", "rabbit", "rabbit", "rabbit"];
     redDice = ["fox", "horse", "pig", "pig", "sheep", "sheep", "rabbit", "rabbit", "rabbit", "rabbit", "rabbit", "rabbit"];
-    players = []
+    players = [];
 
     init() {
         //todo welcome screen where user can set num of players
@@ -41,7 +41,7 @@ class Game {
             return document.querySelector('.modal__content').innerHTML = ''
         }
         function createPlayersNamesInput(num = 1) {
-            let output = ''
+            let output = '';
             for (let i = 0; i < num; i++) {
                 output += `<div class="player__input player__name">
                 <label for="player${i}Name">Player ${i + 1} name:</label>
@@ -52,39 +52,39 @@ class Game {
         }
 
         const setPlayerName = (e) => {
-            e.preventDefault() //no reload on submit
-            const playerInput = e.target.querySelector('input') //get input
-            const playerName = playerInput.value
+            e.preventDefault(); //no reload on submit
+            const playerInput = e.target.querySelector('input'); //get input
+            const playerName = playerInput.value;
             // create new player using
-            if (!playerName) return
-            this.players.push(new Player(playerName, false))
-            playerInput.value = ''
+            if (!playerName) return;
+            this.players.push(new Player(playerName, false));
+            playerInput.value = '';
             // show instructions
-            clearModalContent()
+            clearModalContent();
             this.showInstructions()
-        }
+        };
 
-        const welcomeMessage = 'Please type your name'
-        const modalContent = document.createElement('div')
-        modalContent.classList.add('modal__content--initial', 'modal__content')
+        const welcomeMessage = 'Please type your name';
+        const modalContent = document.createElement('div');
+        modalContent.classList.add('modal__content--initial', 'modal__content');
         const modalHTMLContent = `<form class="modal__form modal__form--playerName">
         ${createPlayersNamesInput()}
         <button type="submit" class="modal__btn" id="submitPlayerName">confirm</button>
         </form>
-        `
-        modalContent.innerHTML = modalHTMLContent
-        this.showModal(welcomeMessage, modalContent)
-        const btnListener = () => document.querySelector('form.modal__form--playerName').addEventListener('submit', setPlayerName)
+        `;
+        modalContent.innerHTML = modalHTMLContent;
+        this.showModal(welcomeMessage, modalContent);
+        const btnListener = () => document.querySelector('form.modal__form--playerName').addEventListener('submit', setPlayerName);
         btnListener()
     }
 
     showModal(title = 'exchange animals', content = document.createElement('')) {
-        const modalWindow = document.querySelector('article.modal__window')
-        const modalClose = modalWindow.querySelector('button.modal__close')
-        const modalHeader = modalWindow.querySelector('h2.modal__header')
-        modalWindow.lastElementChild.appendChild(content)
-        modalHeader.innerText = title
-        modalWindow.style.display = 'flex'
+        const modalWindow = document.querySelector('article.modal__window');
+        const modalClose = modalWindow.querySelector('button.modal__close');
+        const modalHeader = modalWindow.querySelector('h2.modal__header');
+        modalWindow.lastElementChild.appendChild(content);
+        modalHeader.innerText = title;
+        modalWindow.style.display = 'flex';
         modalClose.addEventListener('click', (this.closeModal))
     }
 
@@ -95,7 +95,7 @@ class Game {
     throwDices() {
 
         function throwDice(dice) {
-            const equation = Math.abs(Math.floor(Math.random() * dice.length - 1))
+            const equation = Math.abs(Math.floor(Math.random() * dice.length - 1));
             return dice[equation] === undefined ? throwDice(dice) : dice[equation]
         }
 
@@ -103,8 +103,8 @@ class Game {
 
             function grow(animalName, addOne = false) {
 
-                const animalNum = player.animals[animalName]
-                console.log('animalN', animalNum, addOne)
+                const animalNum = player.animals[animalName];
+                console.log('animalN', animalNum, addOne);
                 if (addOne && animalNum === 0 || animalNum <= 1) {
                     return player.animals[animalName]++
                 }
@@ -112,43 +112,43 @@ class Game {
                     return player.animals[animalName] += (player.animals[animalName] + 1) / 2
                 }
                 if (addOne && player.animals[animalName] > 1 && player.animals[animalName % 2 !== 0]) {
-                    return player.animals[animalName] += parseInt((player.animals[animalName] - 1) / 2 + 1)
+                    return player.animals[animalName] += (parseInt((player.animals[animalName]) )- 1) / 2 + 1
                 }
                 if (addOne && player.animals[animalName] > 1) {
-                    return player.animals[animalName] += parseInt(player.animals[animalName] / 2 + 1)
+                    return player.animals[animalName] += parseInt(player.animals[animalName]) / 2 + 1
                 }
-                return console.log(player.animals[animalName] += parseInt(player.animals[animalName] / 2))
+                return console.log(player.animals[animalName] += parseInt(player.animals[animalName]) / 2)
             }
 
             switch (true) {
                 case dice1 === 'wolf' && dice2 === 'fox':
-                    wolfAttack()
-                    foxAttack()
-                    break
+                    wolfAttack();
+                    foxAttack();
+                    break;
                 case dice1 === 'wolf' && player.animals[dice2] > 0:
-                    grow(dice2)
-                    wolfAttack()
-                    break
+                    grow(dice2);
+                    wolfAttack();
+                    break;
                 case dice1 === 'wolf' && player.animals[dice2] === 0:
-                    wolfAttack()
-                    break
+                    wolfAttack();
+                    break;
                 case dice2 === 'fox' && player.animals[dice1] > 0:
-                    grow(dice1)
-                    foxAttack()
-                    break
+                    grow(dice1);
+                    foxAttack();
+                    break;
                 case dice2 === 'fox' && player.animals[dice1] === 0:
-                    foxAttack()
-                    break
-                case dice1 === dice2: grow(dice1, true)
-                    console.log('dice match')
-                    break
+                    foxAttack();
+                    break;
+                case dice1 === dice2: grow(dice1, true);
+                    console.log('dice match');
+                    break;
                 case player.animals[dice1] > 0 && player.animals[dice2] > 0:
-                    grow(dice1)
-                    grow(dice2)
-                    break
-                case player.animals[dice1] > 0: grow(dice1)
-                    break
-                case player.animals[dice2] > 0: grow(dice2)
+                    grow(dice1);
+                    grow(dice2);
+                    break;
+                case player.animals[dice1] > 0: grow(dice1);
+                    break;
+                case player.animals[dice2] > 0: grow(dice2);
                     break
             }
 
@@ -172,7 +172,7 @@ class Game {
             The main goal is to breed, exchange and gather different animals (rabbit, sheep, pig, cow, horse and two kinds of dogs)
             and the winner is a person who can gather at least one of each kind (except the dogs). But be careful there are foxes
             around trying to snatch your rabbits and wolves which can eat all your other animals (that is why you need dogs to protect your herd).
-            </p>`
+            </p>`;
 
         const instructionsP2 = `<p>The turn of each player consists of two actions: first you have a possibility
             to exchange your animals according to the special table (e.g for six rabbits you get one sheep, three pigs for a cow)
@@ -180,14 +180,14 @@ class Game {
             You count the number of pairs of the animals on rolled dice (together with the ones on the farm) and this is how many new animals you get,
             divided by two. The trick is that there is a fox on one dice and the wolf which are eating your animals.
             </p>
-        `
+        `;
 
-        const instructionBtn = '<button id="instructionCloseBtn">Close</button>'
-        const modalContent = document.createElement('div')
+        const instructionBtn = '<button id="instructionCloseBtn">Close</button>';
+        const modalContent = document.createElement('div');
 
-        modalContent.classList.add('modal__content', 'modal__content--instruction')
-        modalContent.innerHTML = instructionsP1 + instructionsP2 + instructionBtn
-        this.showModal('Instructions', modalContent)
+        modalContent.classList.add('modal__content', 'modal__content--instruction');
+        modalContent.innerHTML = instructionsP1 + instructionsP2 + instructionBtn;
+        this.showModal('Instructions', modalContent);
         document.getElementById('instructionCloseBtn').addEventListener('click', this.closeModal)
     }
 
@@ -208,7 +208,7 @@ class Game {
 
     foxAttack() {
 
-        console.log('fox attacks')
+        console.log('fox attacks');
 
         if (player.animals.sDog) {
             return player.animals.sDog--
@@ -218,5 +218,5 @@ class Game {
 
 }
 
-const start = new Game
-start.init()
+const start = new Game;
+start.init();
